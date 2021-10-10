fabe4aa7f7fa3a08ebcb37db117410e5c3518bef
Keven Laptop <k.fazio@knights.ucf.edu>
2021-10-10 09:59:01 -0400

    added logic

--- exercise41/baseline.Solution41/src/main/java/exercise41/baseline/Solution41.java
+++ exercise41/baseline.Solution41/src/main/java/exercise41/baseline/Solution41.java
@@ -52,20 +52,28 @@ public class Solution41 {
 
         //scan in the file into a list
         Scanner inputTxt = new Scanner(path);
-        List<String> namesList = new ArrayList<>();
+        List<String> namesList = new ArrayList<>(app.scanInFile(inputTxt));
 
         SortList list1 = new SortList(namesList);
 
         //Get the list of sorted names
-        List<String> sortedNamesList = new ArrayList<>();
+        List<String> sortedNamesList = new ArrayList<>(list1.getSortedList(namesList));
 
         //create new file and print the sorted list
+        app.printToFile(sortedNamesList);
 
     }
 
     public List<String> scanInFile (Scanner fileScanner) {
         //try if the scanner has a next line and if it does add each line to List
         //return the List
+        List<String> namesFile = new ArrayList<>();
+        try(fileScanner) {
+            while (fileScanner.hasNextLine()) {
+                namesFile.add(fileScanner.nextLine());
+            }
+        }
+        return namesFile;
     }
 
     private void printToFile (List<String> output) throws IOException {
@@ -77,5 +85,39 @@ public class Solution41 {
         //write string to file
         //print a message to console if it was written correctly
 
+        BufferedWriter buffWriter = null;
+
+        try {
+            String header = "Total of " + output.size() + " names\n" + "-----------------\n";
+            //Specify the file name and path here
+            File file = new File("data/exercise41_output.txt");
+
+            //make sure that the file exists, if not it will create it.
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            buffWriter = new BufferedWriter(fileWriter);
+            buffWriter.write(header);
+
+            for (int i = 0; i < output.size(); i++){
+                buffWriter.write(output.get(i) + "\n");
+            }
+
+            System.out.println("File written Successfully");
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        finally
+        {
+            try{
+                if(buffWriter!=null)
+                    buffWriter.close();
+            }catch(Exception ex){
+                System.out.println("Error in closing the BufferedWriter"+ex);
+            }
+        }
     }
 }
--- exercise41/baseline.Solution41/src/main/java/exercise41/baseline/SortList.java
+++ exercise41/baseline.Solution41/src/main/java/exercise41/baseline/SortList.java
@@ -1,5 +1,7 @@
 package exercise41.baseline;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortList {
@@ -13,9 +15,12 @@ public class SortList {
 
     public List<String> getSortedList(List<String> namesList){
         //create new list to prevent modifying original list
+        List<String> copyList = new ArrayList<>(namesList);
 
         //compare each string in the list and sort by the last name.
+        copyList.sort(Comparator.comparing(String::toString));
 
         //return the sorted list
+        return copyList;
     }
 }
